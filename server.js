ced1e21c7620c73cba56dc5493805b9bd9aa2459
tlburchh <37091749+tlburchh@users.noreply.github.com>
2018-09-04 16:15:05 -0400

    Created server access script

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+//sets up Expresss app with option of Heroku hosting
+var PORT = process.env.PORT || 7777;
+var app = express();
+
+//sets up Express to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
