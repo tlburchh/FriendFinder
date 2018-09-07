adc1aba3d6098f94a20ec7c4a41547da97c8805e
tlburchh <37091749+tlburchh@users.noreply.github.com>
2018-09-07 17:18:04 -0400

    completed server.js script

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
-//sets up Expresss app with option of Heroku hosting
+//sets up Express app with option of Heroku hosting
 var PORT = process.env.PORT || 7777;
 var app = express();
 
 //sets up Express to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
+
+//serve static assets (if added)
+app.use(express.static(__dirname + "app/public"));
+
+//connect to path files
+var htmlRoutes = require("./app/routing/htmlRoutes")(app, path);
+var apiRoutes = require("./app/routing/apiRoutes")(app, path);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
