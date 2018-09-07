66502b08b00609ada792454af8421e9fb7828938
tlburchh <37091749+tlburchh@users.noreply.github.com>
2018-09-07 17:41:40 -0400

    added htmlRoutes.js script

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,12 +1,11 @@
 //basic routes for home and for survey pages
 
-//NEED TO ADD IF/ELSE STATEMENT
-
-
+module.exports = function(app, path){
 app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "../home.html"))
+    res.sendFile(path.join(__dirname, "../public/home.html"))
 });
 
 app.get("/survey", function(req, res){
-    res.sendFile(path.join(__dirname, "survey.html"))
-})
+    res.sendFile(path.join(__dirname, "../public/survey.html"))
+});
+}
